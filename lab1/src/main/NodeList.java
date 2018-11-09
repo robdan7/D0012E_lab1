02a68da0c0281bf212dc689dea0f8d9bd4cd36b1
Robin <Robin@Robin-Dator>
2018-11-09 20:03:16 +0100

    A crude version of quick-sort is up and running.

--- lab1/src/main/NodeList.java
+++ lab1/src/main/NodeList.java
@@ -1,22 +1,24 @@
 package main;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * This class represents a list containing linked nodes. Every node has a next and a previous node.
  * @author Robin
  *
  */
-public class NodeList<A> implements Iterable<A> {
-	private Node start, end;
+public class NodeList<A> implements Iterable<NodeList.Node<A>> {
+	private Node<A> start, end;
 	private int size = 0;
 	
 	public NodeList() {
-		this.start = new Node(null);
-		this.end = new Node(null);
+		this.start = new Node<A>(null);
+		this.end = new Node<A>(null);
 		this.start.setNext(this.end);
 		this.end.setPrevious(this.start);
 	}
+	
 	
 	/**
 	 * Add an element to the end of the list.
@@ -24,7 +26,7 @@ public class NodeList<A> implements Iterable<A> {
 	 */
 	public void appendEnd(A item) {
 		this.size ++;
-		this.end.insertPrevious(new Node(item));
+		this.end.insertPrevious(new Node<A>(item));
 	}
 	
 	/**
@@ -33,33 +35,96 @@ public class NodeList<A> implements Iterable<A> {
 	 */
 	public void appendStart(A item) {
 		this.size ++;
-		this.start.insertNext(new Node(item));
+		this.start.insertNext(new Node<A>(item));
+}
+	
+	public Node<A> findNode(int i) {
+		if (i >= this.getSize() || this.getSize() == 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		return this.start.getNext().findNodeIndex(i);
+	}
+	
+	/**
+	 * Set this list to be a subset of a larger list. The nodes are the same,
+	 * but this list has only access to the nodes within this list.
+	 * @param start
+	 * @param end
+	 */
+	public NodeList<A> createListSubset(Node<A> start, Node<A> end) {
+		NodeList<A> list = new NodeList<A>();
+		
+		int i = 1;
+		Node<A> n = start;
+		
+
+		while (n != null && !n.equals(end)) {
+				i ++;
+				n = n.getNext();
+		}
+		
+		list.size = i;
+
+		//list.first = start;
+		//list.last = end;
+		
+		start.insertPrevious(list.start);
+		end.insertNext(list.end);
+		//this.size += 2;
+		return list;
+	}
+	
+	/**
+	 * @return The first element in the list.
+	 */
+	public Node<A> getFirst() {
+		if (this.isEmpty()) {
+			throw new IndexOutOfBoundsException("the array is empty");
+		}
+		return this.start.getNext();
+	}
+	
+	/**
+	 * @return The last element in the list.
+	 */
+	public Node<A> getLast() {
+		if (this.isEmpty()) {
+			throw new IndexOutOfBoundsException("the array is empty");
+		}
+		return this.end.getPrevious();
 	}
 	
 	/**
 	 * Remove the first element from the list.
 	 * @return
 	 */
-	@SuppressWarnings("unchecked")
 	public A rmStart() {
-		if (this.getSize() == 0) {
+		if (this.isEmpty()) {
 			throw new IndexOutOfBoundsException();
 		}
 		this.size --;
 		return (A) this.start.removeNext().getValue();
-	}
+}
 	
 	/**
 	 * Remove the last element from the list.
 	 * @return
 	 */
-	@SuppressWarnings("unchecked")
 	public A rmEnd() {
-		if (this.getSize() == 0) {
+		if (this.isEmpty()) {
 			throw new IndexOutOfBoundsException();
 		}
 		this.size --;
 		return (A) this.end.removePrevious().getValue();
+}
+	
+	public A removeNode(Node<A> n) {
+		if (this.isEmpty()) {
+			throw new IndexOutOfBoundsException();
+		}
+		n.removeThis();
+		this.size --;
+		return n.getValue();
 	}
 	
 	/**
@@ -70,24 +135,35 @@ public class NodeList<A> implements Iterable<A> {
 		return this.size;
 	}
 	
+	public boolean isEmpty() {
+		return this.getSize() == 0;
+	}
+	
 	@Override
-	public Iterator iterator() {
-		Iterator n = new Iterator() {
-			Node n = start;
+	public Iterator<NodeList.Node<A>> iterator() {
+		Iterator<Node<A>> iterator = new Iterator<Node<A>>() {
+			Node<A> n = start;
 			@Override
 			public boolean hasNext() {
 				return !n.getNext().equals(end);
 			}
 
 			@Override
-			public Object next() {
-				
+			public Node<A> next() {
+				if (!this.hasNext()) {
+					throw new NoSuchElementException();
+				}
 				n = n.getNext();
-				return n.getValue();
+				return n;
 			}
 			
 		};
-		return n;
+		return iterator;
+	}
+	
+	@Override
+	public String toString() {
+		return "{" + this.start.getNext().toString() + "}";
 	}
 	
 	/**
@@ -96,11 +172,11 @@ public class NodeList<A> implements Iterable<A> {
 	 *
 	 * @param <E> - The object type to use.
 	 */
-	private static class Node<E> {
+	protected static class Node<E> {
 		private E value;
-		private Node next, previous;
+		private Node<E> next, previous;
 		
-		Node(E value , Node next, Node previous) {
+		Node(E value , Node<E> next, Node<E> previous) {
 			this.next = next;
 			this.previous = previous;
 			this.value = value;
@@ -114,31 +190,48 @@ public class NodeList<A> implements Iterable<A> {
 		 * Remove the next node in the chain.
 		 * @return The next node in the chain.
 		 */
-		Node removeNext() {
-			Node n = this.getNext();
-			this.setNext(n.getNext());
-			return n;
+		Node<E> removeNext() {
+			if (this.hasNext()) {
+				Node<E> n = this.getNext();
+				this.setNext(n.getNext());
+				return n;
+			}
+			return null;
 		}
 		
 		/**
 		 * Remove the previous node in the chain.
 		 * @return the previous node in the chain.
 		 */
-		Node removePrevious() {
-			Node n = this.getPrevious();
-			this.setPrevious(n.getPrevious());
-			return n;
+		Node<E> removePrevious() {
+			if (this.hasPrevious()) {
+				Node<E> n = this.getPrevious();
+				this.setPrevious(n.getPrevious());
+				return n;
+			}
+			return null;
 		}
 		
+		/**
+		 * Remove this node from the chain. Do not remove this node while iterating over it.
+		 * @return
+		 */
+		Node<E> removeThis() {
+			this.getNext().setPrevious(this.getPrevious());
+			this.getPrevious().setNext(this.getNext());
+			return this;
+		}
 		
 		/**
 		 * Insert a previous node. The chain is preserved.
 		 * @param n - the new node to insert.
 		 */
-		void insertPrevious(Node n) {
+		void insertPrevious(Node<E> n) {
 			n.setPrevious(this.getPrevious());
 			n.setNext(this);
-			this.getPrevious().setNext(n);
+			if (this.hasPrevious()) {
+				this.getPrevious().setNext(n);
+			}
 			this.setPrevious(n);
 			//this.previous = n;
 		}
@@ -147,10 +240,12 @@ public class NodeList<A> implements Iterable<A> {
 		 * Insert a new node after this one. The chain is preserved.
 		 * @param n - The new node to insert.
 		 */
-		void insertNext(Node n) {
+		void insertNext(Node<E> n) {
 			n.setNext(this.getNext());
 			n.setPrevious(this);
-			this.getNext().setPrevious(n);
+			if (this.hasNext()) {
+				this.getNext().setPrevious(n);
+			}
 			this.setNext(n);
 			//this.next = n;
 		}
@@ -159,7 +254,7 @@ public class NodeList<A> implements Iterable<A> {
 		 * Override the previous node.
 		 * @param n
 		 */
-		void setPrevious(Node n) {
+		void setPrevious(Node<E> n) {
 			this.previous = n;
 		}
 		
@@ -167,7 +262,7 @@ public class NodeList<A> implements Iterable<A> {
 		 * Override the next node.
 		 * @param n
 		 */
-		void setNext(Node n) {
+		void setNext(Node<E> n) {
 			this.next = n;
 		}
 		
@@ -175,15 +270,23 @@ public class NodeList<A> implements Iterable<A> {
 		 * Get the next node.
 		 * @return
 		 */
-		Node getNext() {
+		Node<E> getNext() {
 			return this.next;
+		}
+		
+		boolean hasNext() {
+			return this.next != null;
+		}
+		
+		boolean hasPrevious() {
+			return this.previous != null;
 		}
 		
 		/**
 		 * Get the previous node.
 		 * @return
 		 */
-		Node getPrevious() {
+		Node<E> getPrevious() {
 			return this.previous;
 		}
 		
@@ -194,5 +297,46 @@ public class NodeList<A> implements Iterable<A> {
 		E getValue() {
 			return this.value;
 		}
+		
+		void setValue(E item) {
+			this.value = item;
+		}
+		
+		/**
+		 * Find a node based on index.
+		 * @param index
+		 * @return
+		 */
+		Node<E> findNodeIndex(int index) {
+			if (index == 0 && this.getValue() != null) {
+				return this;
+			}
+			if (this.hasNext()) {
+				return this.getNext().findNodeIndex(--index);
+			}
+			return null;
+		}
+		
+		int findIndex(Node<E> n ) {
+			if (this.equals(n)) {
+				return 0;
+			}
+			if (this.hasNext()) {
+				return 1 + this.getNext().findIndex(n);
+			}
+			return 0;
+		}
+	
+		@Override
+		public String toString() {
+			if (this.hasNext()) {
+				return this.getValue() + ";" + this.getNext().toString();
+			}
+			if (this.getValue() != null) {
+				return "" + this.getValue();
+			}
+			return "";
+		}
 	}
+	
 }
--- lab1/src/main/QuickSort.java
+++ lab1/src/main/QuickSort.java
@@ -1,41 +1,127 @@
 package main;
 
-import java.math.*;;
+import java.util.Random;
+import java.util.Iterator;
+
+import main.NodeList.*;
 
 /**
- * Class for quicksort operations.
+ * Class for quick-sort operations.
  * @author Robin
  *
  */
 public class QuickSort {
+	private static int pivotIndex;	// this is used to keep track of the pivot node.
+	
+	public static void main(String[] args) {
+		NodeList<Integer> list = new NodeList<Integer>();
+		Random r = new Random();
+		for (int i = 0; i < 30; i++) {
+			list.appendEnd(r.nextInt(1000));
+		}
+		sort(list);
+		clean(list);
+		System.out.println(list);
+	}
+	
+	/**
+	 * Dirty cleanup function. We need to remove this.
+	 * @param list
+	 */
+	private static void clean(NodeList<Integer> list) {
+		for (Node<Integer> n : list) {
+			if (n.getValue() == null && !(n.equals(list.getFirst().getPrevious()) || n.equals(list.getLast().getNext()))) {
+				//list.removeNode(n);
+				n.removeThis();
+			}
+		}
+	}
+	
 	
 	/**
 	 * Sort a list using quick-sort. The pivot is chosen randomly.
 	 * @param list - The list to sort.
 	 * @return
 	 */
-	public static int[] sort(int[] list) {
-		int pivot = (int)(Math.random()*list.length);	// create a random pivot.
-		return sort(list, pivot);
+	public static void sort(NodeList<Integer> list) {
+		pivotIndex = (int)(Math.random()*list.getSize());	// create a random pivot.
+		
+		Node<Integer> pivotNode = list.findNode(pivotIndex);
+		
+		lazySort(list, pivotNode.getValue());
+		
+		if (list.getSize() <= 2) {				// the list is too small for sorting.
+			return;
+		}
+		
+		
+		if (pivotIndex == 0) {													// the pivot is on index 0
+			sort(list.createListSubset(pivotNode.getNext(), list.getLast()));
+			
+		} else if (pivotIndex == (list.getSize()-1)) {							// the pivot is on the last index.
+			sort(list.createListSubset(list.getFirst(), pivotNode.getPrevious()));
+		}
+		
+		else {
+			sort(list.createListSubset(list.getFirst(), pivotNode.getPrevious()));
+			sort(list.createListSubset(pivotNode.getNext(), list.getLast()));
+		
+		}
 	}
 	
-	
 	/**
-	 * Sort a list using quick-sort.
+	 * Sort a list using quick-sort with fixed pivot.
 	 * @param list - The list to sort.
 	 * @param pivot - Pivot element.
 	 * @return
 	 */
 	public static int[] sort(int[] list, int pivot) {
-		
-		if (list.length == 1) {
-			return list;								// return statement.
-		}
-		
-
-		
-		
+		//TODO make it work somehow.
 		return null;
 	}
 
+	
+
+	/**
+	 * Sort a {@link NodeList} with a size larger than 1. Every integer larger than the pivot is put last, and
+	 * every integer smaller is put first.
+	 * @param list - The list to sort
+	 * @param pivot - the pivot number.
+	 * @return
+	 */
+	private static void lazySort(NodeList<Integer> list, int pivot) {
+		if (list.getSize() <= 1) {
+			return;
+		}
+		
+		Iterator<Node<Integer>> iterator = list.iterator();
+		Node<Integer> n = iterator.next();
+		int previousValue = n.getValue();
+		int newPivotIndex = pivotIndex;
+		
+		for (int i = 1; i < list.getSize(); i++) {		// N.B. Start on index 1
+			n = iterator.next();			// skip ahead one node and perform operations on the previous one.
+			
+			
+			// large numbers are move to higher index and small number are move to lower index,
+			// but only if the are on the wrong "side" of the pivot.
+			if (i-1 < pivotIndex && previousValue > pivot) {	// put large numbers after pivot
+				list.appendEnd(list.removeNode(n.getPrevious()));
+				newPivotIndex --;
+			} else if (i-1 > pivotIndex && previousValue < pivot) {		// put small numbers before pivot.
+				list.appendStart(list.removeNode(n.getPrevious()));
+				newPivotIndex ++;
+			}		
+			
+			previousValue = n.getValue();	// update the previous value for next iteration.
+		}
+		
+		if (list.getSize()-1 > pivotIndex && previousValue < pivot) {		// one extra if for the last index.
+			list.appendStart(list.removeNode(n));
+			newPivotIndex ++;
+		}
+		
+		// update pivot index.
+		pivotIndex = newPivotIndex;
+	}
 }
