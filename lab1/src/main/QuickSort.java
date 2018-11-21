123212f16362a0c8b7b98ec7768e1c12517e8569
Oskar <Oskar@>
2018-11-21 03:06:20 +0100

    Quicksort som fungerar fint.

--- lab1/src/main/QuickSort.java
+++ lab1/src/main/QuickSort.java
@@ -74,10 +74,61 @@ public class QuickSort {
 	 * @param pivot - The pivot element.
 	 * @return
 	 */
+	/*
 	public static void sort(int[] list, int pivot) {
 		// TODO make it work somehow.
 	}
-
+*/
+	
+	
+	
+//	Fungerande quicksortkod typ , work in progress.
+	
+	/**
+	 * @author Oskar
+	 * @param list
+	 * @param lowIndex
+	 * @param highIndex
+	 */
+	public void sort(int[] list, int lowIndex, int highIndex) {
+		if(lowIndex < highIndex) {
+			int partitionIndex = partition(list, lowIndex, highIndex);
+			 // Sortera vänster och höger om partitionIndex allstå PIVOT.
+			sort(list, lowIndex, partitionIndex-1);
+			sort(list, partitionIndex-1, highIndex);
+			
+		}
+		
+	}
+	
+	/**
+	 * @author Oskar
+	 * @param list
+	 * @param lowIndex
+	 * @param highIndex
+	 * @return
+	 */
+	private int partition(int[] list, int lowIndex, int highIndex) {
+		int pivot = list[highIndex];
+		int index = lowIndex-1;
+		for(int i = lowIndex; i<highIndex;i++) {
+			if(list[i] <= pivot) {
+				index++;
+				//Swap
+				int temp = list[index+1];
+				list[index+1] = list[i];
+				list[i] = temp;
+			}
+			
+			
+		}
+		int temp = list[index+1];
+		list[index+1] = list[highIndex];
+		list[highIndex] = temp;
+		
+		return index+1;
+		
+	}
 	/**
 	 * Put Large elements after the pivot and small ones before the pivot.
 	 * 
